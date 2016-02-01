6c98b559daf48ad129709810e90be52817bc9557
Insidexa <nostresss77@gmail.com>
2016-02-01 23:41:23 +0200

    create Validator and Filters

--- framework/Validation/Filter/Length.php
+++ framework/Validation/Filter/Length.php
@@ -11,5 +11,32 @@ namespace Framework\Validation\Filter;
 
 class Length
 {
+	private $error = '';
 
+	private $min;
+
+	private $max;
+
+	public function __construct($min, $max) {
+		$this->min = $min;
+		$this->max = $max;
+	}
+
+	public function getErrors () {
+		return $this->error;
+	}
+
+	public function checkInput ($nameField, $value) {
+
+		$length = strlen($value);
+
+		if ($length < $this->min || $length > $this->max) {
+			$this->error = 'Field ' . $nameField . ' must not be less than ' . $this->min
+				. ' symbols and not larger than ' . $this->max;
+
+			return false;
+		}
+
+		return true;
+	}
 }
--- framework/Validation/Validator.php
+++ framework/Validation/Validator.php
@@ -11,5 +11,41 @@ namespace Framework\Validation;
 
 class Validator
 {
+	private $errors = [];
+	private $model;
 
+	public function __construct($model) {
+		$this->model = $model;
+
+		$this->check();
+	}
+
+	private function check () {
+
+		$rulesModel = $this->model->getRules();
+
+		foreach ($rulesModel as $properties => $rules) {
+
+			foreach ($rules as $filter) {
+				$result = $filter->checkInput($this->model->$properties);
+				if (!$result)
+					$this->errors[$properties] .= "<br>" . $filter->getErrors();
+			}
+
+		}
+
+	}
+
+	public function getErrors () {
+		return $this->errors;
+	}
+
+	public function isValid () {
+		$isErrors = false;
+		if (count($this->errors) === 0) {
+			$isErrors = true;
+		}
+
+		return $isErrors;
+	}
 }
