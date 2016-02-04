bca4d70974f9add9785a1bb1f04bb07a38beb756
Insidexa <nostresss77@gmail.com>
2016-02-04 22:08:30 +0200

    add Dispatcher

--- framework/Router/Dispatcher.php
+++ framework/Router/Dispatcher.php
@@ -9,7 +9,37 @@
 
 namespace Framework\Router;
 
-
+/**
+ * Class Dispatcher
+ *
+ * @package Framework\Router
+ */
 class Dispatcher {
+
+	/**
+	 * @param      $controller
+	 * @param      $method
+	 * @param null $arguments
+	 *
+	 * @return mixed
+	 * @throws \ErrorException | \BadMethodCallException
+	 */
+	public static function create ($controller, $method, $arguments = null) {
+
+		$methodName = $method . 'Action';
+
+		if (!class_exists($controller)) {
+			throw new \ErrorException('Class ' . $controller . ' not found');
+		}
+
+		$controllerObj = new $controller;
+
+		if (!method_exists($controllerObj, $methodName)) {
+			throw new \BadMethodCallException('Method ' . $methodName . ' not found in ' . $controller);
+		}
+
+		return $controllerObj->$methodName($arguments);
+
+	}
 
 }
