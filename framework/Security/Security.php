370e37925162924b6427c39626f236f3e774485d
Insidexa <nostresss77@gmail.com>
2016-02-05 15:36:51 +0200

    add methods for Security

--- framework/Security/Security.php
+++ framework/Security/Security.php
@@ -8,8 +8,95 @@
  */
 
 namespace Framework\Security;
+use Framework\DI\Service;
 
-
+/**
+ * Class Security
+ *
+ * @package Framework\Security
+ */
 class Security {
+
+	/**
+	 * Security constructor.
+	 */
+	public function __construct() {
+
+		$this->getToken();
+
+	}
+
+	public function acl ($role) {
+
+		$user = $this->getUser();
+
+		if ($role !== $user->role) {
+			throw new \Exception('Permission denied');
+		}
+
+	}
+
+	public function getToken () {
+
+		$token = Service::get('session')->get('token');
+
+		if (!empty($token)) return $token;
+
+		return $this->newToken();
+
+	}
+
+	private function newToken () {
+
+		return Service::get('session')->set('token', md5(random_int(0, 1000)));
+
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isAuthenticated () {
+
+		$flag = false;
+
+		if (Service::get('session')->get('user') !== false) {
+
+			$flag = true;
+
+		}
+
+		return $flag;
+	}
+
+	/**
+	 * @param $model
+	 */
+	public function setUser ($model) {
+
+		Service::get('session')->set('user', $model);
+
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getUser () {
+
+		$user = Service::get('session')->get('user');
+
+		if (Service::get('session')->get('user') === false) {
+			$user = null;
+		}
+
+		return $user;
+
+	}
+
+
+	public function clear () {
+
+		Service::get('session')->destroy();
+
+	}
 
 }
