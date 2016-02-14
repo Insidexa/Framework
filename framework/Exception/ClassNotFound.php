0fc3f64884743fd5f99fa5f668e33d2ce8b83e03
Insidexa <nostresss77@gmail.com>
2016-02-14 15:46:02 +0200

    add AuthLogin and ClassNotFound exception

---
+++ framework/Exception/ClassNotFound.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jashka
+ * Email: nostresss77@gmail.com
+ * Date: 14.02.16
+ * Time: 15:33
+ */
+
+namespace Framework\Exception;
+
+
+class ClassNotFound extends \Exception{
+
+}
