5f5d8c1b429c8f1b8f0ef618e57cc6d728eece56
Insidexa <nostresss77@gmail.com>
2016-03-13 21:54:19 +0200

    add view for ProfileController

--- src/CMS/views/Profile/profile.html.php
+++ src/CMS/views/Profile/profile.html.php
@@ -1,10 +1,30 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-
-</body>
-</html>
+<div class="col-md-offset-2 col-md-8">
+	<form role="form" method="post" action="<?php echo $action ?>">
+		<div style="display: none" class="form-group">
+			<label for="id" class="col-sm-2 control-label">Email</label>
+			<div class="col-sm-10">
+				<input type="hidden" id="id" value="<?php echo $user->id ?>" name="id">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="email" class="col-sm-2 control-label">Email</label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="email" placeholder="Email"
+				       value="<?php echo $user->email ?>" name="email">
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="password" class="col-sm-2 control-label">Password</label>
+			<div class="col-sm-10">
+				<input type="password" class="form-control" id="password" placeholder="Password"
+				       value="<?php echo $user->password ?>" name="password">
+			</div>
+		</div>
+		<div class="form-group">
+			<div class="col-sm-offset-2 col-sm-10">
+				<button type="submit" class="btn btn-default">Update</button>
+			</div>
+		</div>
+		<?php $generateToken() ?>
+	</form>
+</div>
