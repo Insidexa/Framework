458adf986ccdc4affa197a0aa159623483cd9a15
Insidexa <nostresss77@gmail.com>
2016-03-13 21:53:57 +0200

    Add ProfileController

--- src/CMS/Controller/ProfileController.php
+++ src/CMS/Controller/ProfileController.php
@@ -9,7 +9,51 @@
 
 namespace CMS\Controller;
 
+use Blog\Model\User;
+use Framework\ {
+	Controller\Controller,
+	DI\Service,
+	Exception\DatabaseException
+};
 
-class ProfileController {
+/**
+ * Class ProfileController
+ *
+ * @package CMS\Controller
+ */
+class ProfileController extends Controller {
+
+	public function getAction () {
+
+		$user = Service::get('security')->getUser();
+
+		return $this->render('profile.html', [
+			'user' => $user,
+			'action' => $this->generateRoute('update_profile')
+		]);
+
+	}
+
+	public function updateAction () {
+
+		$error = [];
+
+		try {
+			User::where([
+				'id' => (int)$this->getRequest()->post('id')
+			])->update([
+				'email' => $this->getRequest()->post('email'),
+				'password' => $this->getRequest()->post('password')
+			]);
+		} catch(DatabaseException $e) {
+			$error[] = $e->getMessage();
+		}
+
+		// Мне логично после апдейта юзера его разлогинивать
+		Service::get('security')->clear();
+
+		return $this->redirect($this->generateRoute('login'), $error);
+
+	}
 
 }
