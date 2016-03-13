32eefe52eaa7f0fa74ed7cd5348f370d81b58f0c
Insidexa <nostresss77@gmail.com>
2016-03-13 21:53:00 +0200

    add Namespace for autoload CMS bundle

---
+++ src/CMS/Controller/ProfileController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jashka
+ * Email: nostresss77@gmail.com
+ * Date: 13.03.16
+ * Time: 19:36
+ */
+
+namespace CMS\Controller;
+
+
+class ProfileController {
+
+}
