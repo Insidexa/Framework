3d7ac2424df7521c5144c1d44e54b6ef7b8dfffa
Insidexa <nostresss77@gmail.com>
2016-03-19 21:43:12 +0200

    add PSR-7 implementing RequestUri

--- framework/Request/RequestUri.php
+++ framework/Request/RequestUri.php
@@ -7,9 +7,103 @@
  * Time: 20:33
  */
 
-namespace Request;
+namespace Framework\Request;
 
+/**
+ * Class RequestUri
+ *
+ * @package Request
+ */
+class RequestUri implements UriInterface {
 
-class RequestUri {
+	protected $scheme;
+
+	protected $host;
+
+	protected $port;
+
+	protected $uri;
+
+	protected $fragment;
+
+	protected $parsedUrl;
+
+	public function __construct() {
+		$this->scheme = array_key_exists('HTTPS', $_SERVER) ? $_SERVER['HTTPS'] : 'http';
+		$this->host = $_SERVER['HTTP_HOST'];
+		$this->port = $_SERVER['SERVER_PORT'];
+		$this->uri = $_SERVER['REQUEST_URI'];
+		$this->parsedUrl = parse_url($this->getUrl());
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getUrl () {
+
+		return $this->scheme . '://' . $this->host . $this->uri;
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getFragment() {
+
+		return $this->parsedUrl['fragment'];
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getHost() {
+
+		return $this->host;
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPath() {
+
+		return $this->parsedUrl['path'];
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getPort() {
+
+		return $this->port;
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getQuery() {
+
+		return $this->uri;
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getScheme() {
+
+		return $this->scheme;
+
+	}
+
+	/**
+	 * @return string
+	 */
+	public function __toString() {
+		return $this->getScheme() . '://' . $this->getHost();
+	}
 
 }
