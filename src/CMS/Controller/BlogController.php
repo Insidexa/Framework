480f282e6e9e2d8a7395c652a43b33f9142e4b70
Insidexa <nostresss77@gmail.com>
2016-03-21 10:41:18 +0200

    edit the post implemented

--- src/CMS/Controller/BlogController.php
+++ src/CMS/Controller/BlogController.php
@@ -13,6 +13,9 @@ use Framework\ {
 	Controller\Controller,
 	Response\Response
 };
+use Blog\Model\Post;
+use Framework\Exception\DatabaseException;
+use Framework\Request\Request;
 
 /**
  * Class BlogController
@@ -21,9 +24,37 @@ use Framework\ {
  */
 class BlogController extends Controller{
 
-	public function editAction ($id) {
+	public function editAction ($id, Request $request) {
 
-		return new Response(__METHOD__ . ': ' . (int)$id);
+		$id = (int)$id;
+		$errors = [];
+
+		if ($request->isPost()) {
+			try {
+				$date = new \DateTime();
+
+				$id = Post::where([
+					'id' => $id
+				])->update([
+					'title' => $request->post('title'),
+					'content' => $request->post('content'),
+					'date' => $date->format('Y-m-d H:i:s')
+				]);
+
+				return $this->redirect($this->generateRoute('home'), 'The data has been updated successfully');
+
+			} catch (DatabaseException $e) {
+				$errors[] = $e->getMessage();
+			}
+		}
+
+		return $this->render('edit.html', [
+			'post' => Post::find($id),
+			'action' => $this->generateRoute('edit_post', [
+				'id' => $id
+			]),
+			'errors' => $errors ?? null
+		]);
 
 	}
 
